d48228a73b5228fabc73b07f24de89eb16bfbab5
Adejare <adejare.arasheed@gmail.com>
2024-11-06 12:30:16 +0100

    ES6 Basics

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,3 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  return array.map((value) => appendString + value);
+  // return array.map((value) => appendString + value);
+  for (const idx in array) {
+    const value = array[idx];
+    array[idx] = appendString + value;
+  }
+
+  return array;
 }
