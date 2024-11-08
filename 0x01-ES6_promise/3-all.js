862c1e526c9b16545aa3003ece35f5f319612d5a
Adejare <adejare.arasheed@gmail.com>
2024-11-08 09:01:43 +0100

    ES6 Promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,17 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return uploadPhoto()
-    .then((result) => result.body)
-    .then((body) => {
-      return createUser()
-        .then((result) => console.log(body, result.firstName, result.lastName));
+  // return uploadPhoto()
+  //   .then((result) => result.body)
+  //   .then((body) => {
+  //     return createUser()
+  //       .then((result) => console.log(body, result.firstName, result.lastName));
+  //   })
+  //   .catch(() => console.error('Signup system offline'));
+
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(photo.body, user.firstName, user.lastName);
     })
     .catch(() => console.error('Signup system offline'));
 }
