ca418a1e6542443db514c19e40e65e6899c5a127
Adejare <adejare.arasheed@gmail.com>
2024-11-08 09:05:58 +0100

    ES6 Promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -11,5 +11,5 @@ export default function handleProfileSignup() {
 
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => console.log(photo.body, user.firstName, user.lastName))
-    .catch(() => console.error('Signup system offline'));
+    .catch(() => new Error('Signup system offline'));
 }
